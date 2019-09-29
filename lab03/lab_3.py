7cef27b39dd0716b989ace8d252e86f7add1e673
uvjar <820672172@qq.com>
2019-09-29 18:15:21 +0100

    lab3

--- lab03/lab_3.py
+++ lab03/lab_3.py
@@ -6,8 +6,8 @@ def question_1():
     #
     # Yes, you could try them each in turn..!
     # But don't do that.
-    return ""   # Placeholder
-    # return "Nearest Neighbors"
+    # return "I don't know"   # Placeholder
+    return "Nearest Neighbors"
     # return "Logistic Regression"
     # return "Linear SVM"
     # return "RBF SVM"
@@ -16,14 +16,14 @@ def question_1():
 def question_2():
     # This function returns a single number,
     # corresponding to the answer.
-    return -1 # Replace with your answer.
+    return 1.35 # Replace with your answer.
 
 def question_3():
     # This function returns a single number,
     # corresponding to the answer.
-    return -1 # Replace with your answer.
+    return 0.062 # Replace with your answer.
 
 def question_4():
     # This function returns a single number, corresponding to the
     # digit most commonly misclassified.
-    return -1 # Replace with your answer.
+    return 7 # Replace with your answer.
